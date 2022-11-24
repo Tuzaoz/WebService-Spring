a0f28ec0fc940053d16d5e080135b6efeb5e5a1a
Arthur Dantas <arthurdantaspro@gmail.com>
2022-11-24 12:15:49 -0300

    Criação do projeto

---
+++ src/main/java/com/tuzao/webservice/WebServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tuzao.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebServiceApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
