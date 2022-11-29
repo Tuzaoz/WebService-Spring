064d3a0f986d2f72c6e1ffe0338033546778d83c
Arthur Dantas <arthurdantaspro@gmail.com>
2022-11-29 18:10:24 -0300

    Criada Entidade Payment e feito associação com orders

--- src/main/java/com/tuzao/webservice/config/TestConfig.java
+++ src/main/java/com/tuzao/webservice/config/TestConfig.java
@@ -65,6 +65,10 @@ public class TestConfig implements CommandLineRunner {
         p4.getCategory().add(cat1);
         p4.getCategory().add(cat3);
         p5.getCategory().add(cat2);
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1 );
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
